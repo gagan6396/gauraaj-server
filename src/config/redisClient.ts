c48b36a11fd4276df84ea9b08c95b8d6243cbe0c
Ravi chandra <githubravi01@gmail.com>
2024-12-07 13:24:20 +0530

    Redis Connection logic

---
+++ src/config/redisClient.ts
@@ -0,0 +1,20 @@
+import { createClient } from "redis";
+
+const redisClient = createClient();
+
+redisClient.on("error", (error) => console.error("Redis Client Error", error));
+redisClient.on("connect", () => console.log("Redis Client Connected"));
+redisClient.on("ready", () => console.log("Redis Client Ready for Commands"));
+
+const connectRedis = async () => {
+  if (!redisClient.isOpen) {
+    try {
+      await redisClient.connect();
+      console.log("Redis Client Connected Successfully");
+    } catch (error) {
+      console.error("Could not connect to Redis", error);
+    }
+  }
+};
+
+export { redisClient, connectRedis };
