f4513c780437c7c32cf088f1c42f5fdd5c9d90bd
rishabh gehlot <rishabh3x@gmail.com>
2025-01-14 22:20:54 +0530

    `Removed file filter middleware from uploadImage.ts`

--- src/utils/uploadImage.ts
+++ src/utils/uploadImage.ts
@@ -21,18 +21,17 @@ const upload = multer({
       cb(null, uniqueFileName); // File name in S3 bucket
     },
   }),
-  fileFilter: (req, file, cb) => {
-    const allowedTypes = ["image/jpeg", "image/png", "image/jpg"];
-    if (allowedTypes.includes(file.mimetype)) {
-      cb(null, true);
-    } else {
-      cb(
-        new Error(
-          `Invalid file type: ${file.mimetype}. Only JPG, JPEG, and PNG are allowed.`
-        )
-      );
-    }
-  },
+  // fileFilter: (req, file, cb) => {
+  //   if (
+  //     file.mimetype === "image/jpeg" ||
+  //     file.mimetype === "image/png" ||
+  //     file.mimetype === "image/jpg"
+  //   ) {
+  //     cb(null, true);
+  //   } else {
+  //     cb(new Error("Invalid file type. Only JPG, JPEG, and PNG are allowed."));
+  //   }
+  // },
   limits: { fileSize: 5 * 1024 * 1024 }, // 5 MB file size limit per file
 });
 
