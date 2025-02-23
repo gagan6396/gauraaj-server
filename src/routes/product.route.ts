62fc4c2c18b9cd9e303e2d1a48987ace7451e031
rishabh gehlot <rishabh3x@gmail.com>
2025-02-23 14:46:44 +0530

    update product routes

--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -1,36 +1,22 @@
-// routes/supplierProduct.routes.ts
 import { Router } from "express";
 import {
-  addProductBySupplier,
-  deleteProductBySupplier,
-  getAllSupplierProducts,
+  filterProduct,
+  getAllProducts,
+  getAllProductsWithOutAuth,
   getProductById,
-  updateProductBySupplier,
-} from "../controllers/supplierProduct.controller";
+  getProductByIdWithOutAuth,
+  searchProduct,
+} from "../controllers/product.controller";
 import authMiddleware from "../middlewares/authMiddleware";
-import handleImageUpload from "../middlewares/imageMiddleware";
 
-const supplierProductRoute = Router();
+const productRoute = Router();
 
-// Define supplier product routes
-supplierProductRoute.post(
-  "/",
-  authMiddleware,
-  handleImageUpload, // Handles image uploads; we'll extend it for video if needed
-  addProductBySupplier
-);
-supplierProductRoute.patch(
-  "/:productId",
-  authMiddleware,
-  handleImageUpload,
-  updateProductBySupplier
-);
-supplierProductRoute.delete(
-  "/:productId",
-  authMiddleware,
-  deleteProductBySupplier
-);
-supplierProductRoute.get("/product-by-id/:productId", getProductById);
-supplierProductRoute.get("/:supplierId", getAllSupplierProducts);
+// Define productRoute
+productRoute.get("/", getAllProductsWithOutAuth);
+productRoute.get("/auth", authMiddleware, getAllProducts);
+productRoute.get("/:productId", getProductByIdWithOutAuth);
+productRoute.get("/auth/:productId", authMiddleware, getProductById);
+productRoute.get("/search/:searchTerm", searchProduct);
+productRoute.get("/filter/:categoryId", filterProduct);
 
-export default supplierProductRoute;
+export default productRoute;
